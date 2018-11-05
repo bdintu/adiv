60cd48b6e98c66380cba8c075da453c0fa5da76f
j <bundit.jay@gmail.com>
2018-11-05 17:41:40 +0700

    Add HashMap Serializable

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,37 +11,57 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main extends Application {
 
-    private static final String filepath="obj";
-    private static ArrayList<user.Model> users;
+    private static final String filepath="adiv.db";
+    private static HashMap<String, ArrayList> map;
+    public static ArrayList<user.Model> users;
+    public static ArrayList<user.Model> skills;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 550));
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
-        Main serObj = new Main();
-        user.Model userss = new user.Model(1, "j@mail.com","j","passwd");
-        ArrayList<user.Model> users = new ArrayList<user.Model>();
-        users.add(userss);
-        serObj.serializeWrite(users);
-        System.out.println("The Object  was succesfully written to a file");
 
-        serializeRead();
-        for(user.Model user : users) {
-            System.out.println("username: " + user.getUsername());
-        }
+        ArrayList<user.Model> users = new ArrayList<user.Model>();
+        user.Model user = new user.Model(1, "j@mail.com","j","passwd");
+        users.add(user);
+
+        ArrayList<skill.Model> skills = new ArrayList<skill.Model>();
+        skill.Model skill = new skill.Model(1, "assembly");
+        skills.add(skill);
+
+        objWrite();
+
+        objRead();
+        for(user.Model u: users)
+            System.out.println(u.getUsername());
+
         launch(args);
     }
 
-    public void serializeWrite(Object serObj) {
+    public static void objWrite() {
+        HashMap<String, ArrayList> map = new HashMap<String, ArrayList>();
+        map.put("user", users);
+        map.put("skill", skills);
+        serializeWrite(map);
+        System.out.println("The Object  was succesfully written to a file");
+    }
+
+    public static void objRead() {
+        serializeRead();
+        users = map.get("user");
+        skills = map.get("skill");
+    }
+
+    public static void serializeWrite(Object serObj) {
 
         try {
             FileOutputStream fileOut = new FileOutputStream(filepath);
@@ -59,7 +79,7 @@ public class Main extends Application {
         try {
             FileInputStream fileIn = new FileInputStream(filepath);
             ObjectInputStream objectIn = new ObjectInputStream(fileIn);
-            users = (ArrayList<user.Model>)objectIn.readObject();
+            map = (HashMap<String, ArrayList>)objectIn.readObject();
             objectIn.close();
             fileIn.close();
         } catch (Exception ex) {
--- src/skill/Model.java
+++ src/skill/Model.java
@@ -1,6 +1,29 @@
 package skill;
 
-public class Model {
+import java.io.Serializable;
+
+public class Model implements Serializable {
     private int skillId;
     private String name;
+
+    public Model(int skillId, String name) {
+        this.skillId = skillId;
+        this.name = name;
+    }
+
+    public int getSkillId() {
+        return skillId;
+    }
+
+    public void setSkillId(int skillId) {
+        this.skillId = skillId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
