976aea88b045f45e0e7c097886a51fd9f9d087d5
j <bundit.jay@gmail.com>
2018-11-12 02:13:32 +0700

    Add FXMLController user

--- src/user/FXMLController.java
+++ src/user/FXMLController.java
@@ -1,8 +1,14 @@
 package user;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,27 +16,42 @@ import java.util.ResourceBundle;
 public class FXMLController implements Initializable {
 
     @FXML
-    private TextField username;
-    private TextField password;
+    private TextField email;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Hyperlink createUserHyperlink;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        username.setText("Fuckkkk youuuu");
-    }
 
-    public String getUsername() {
-        return username.getText();
-    }
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    Stage.stage.changeStage("Home-user");
+                    email.setText("login");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
-    public void setUsername(String username) {
-        this.username.setText(username);
-    }
-
-    public String getPassword() {
-        return password.getText();
-    }
-
-    public void setPassword(String password) {
-        this.password.setText(password);
+        createUserHyperlink.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    Stage.stage.changeStage("Register-user");
+                    email.setText("createUser");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
