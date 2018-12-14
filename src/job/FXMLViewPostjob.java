ad87b1d471a6ad6e679982d2be6be5eab4319261
NuttapatPT <Nuttapat_pt@hotmail.com>
2018-12-14 16:40:51 +0700

    UI

--- src/job/FXMLViewPostjob.java
+++ src/job/FXMLViewPostjob.java
@@ -46,7 +46,7 @@ public class FXMLViewPostjob implements Initializable {
             try {
 
 
-                Stage.stage.changeStage("postJob");
+                Stage.stage.changeStage("ViewJob");
 
 
             } catch (Exception e) {
